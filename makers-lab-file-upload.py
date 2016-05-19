9b90680889445786ab1a8e877335aad3441cf403
jakub_zika <kuba.zika@email.cz>
2016-05-19 10:32:02 +0200

    Add filename checking

--- makers-lab-file-upload.py
+++ makers-lab-file-upload.py
@@ -3,12 +3,12 @@ import sys
 import os
 from flask import Flask, url_for, render_template, request, redirect
 
-
 app = Flask(__name__)
 
 UPLOADS_PATH = 'uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOADS_PATH
 app.debug = True
+
 
 @app.route('/')
 def index():
@@ -18,14 +18,25 @@ def index():
 @app.route('/image_upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
+
         file = request.files["file"]
         filename = file.filename
-        #file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'empty.txt'))
-        path=os.path.normcase(os.path.join(os.path.dirname(__file__), 'uploads/temporary.stl'))
-        print(path)
-        file.save(path)
-        return 'File was succesfully uploaded'
+        if(filename.split('.')[-1]=='stl'):
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'empty.txt'))
+            path = os.path.normcase(os.path.join(os.path.dirname(__file__), 'uploads/temporary.stl'))
+            print(path)
+            try:
+                file.save(path)
+            except(Exception):
+                print('File was not uploaded')
+            return 'File was succesfully uploaded'
 
+
+def executeFromFile(path):
+    with open(path, 'r') as f:
+        command = f.read()
+        command = command.rsplit('\n')
+        print(command)
 
 
 with app.test_request_context():
