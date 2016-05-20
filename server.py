1611086df482d465e63e63d6a0d60ec7c2a8bee0
jakub_zika <kuba.zika@email.cz>
2016-05-20 15:14:08 +0200

    Add three way communication

--- server.py
+++ server.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 UPLOADS_PATH = 'uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOADS_PATH
 
-CURA_SCRIPT_PATH='cura.sh'
+CURA_SCRIPT_PATH = 'cura.sh'
+STREAM_PORT = 4000
 
 app.debug = True
 
@@ -19,46 +20,81 @@ def index():
     return render_template('pages/index.jinja2', )
 
 
-@app.route('/stl-pricing', methods=['POST','GET'])
+@app.route('/stl-pricing', methods=['POST', 'GET'])
 def upload():
     if request.method == 'POST':
         file = request.files["file"]
         filename = file.filename
-        if(filename.split('.')[-1]=='stl'):
+        if (filename.split('.')[-1] == 'stl'):
             # file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'empty.txt'))
             path = os.path.normcase(os.path.join(os.path.dirname(__file__), 'uploads/temporary.stl'))
             try:
                 file.save(path)
             except(Exception):
                 return 'File was not uploaded'
-            #executeFromFile(filename)
+            # executeFromFile(filename)
             return render_template('pages/processing.jinja2', filename=filename)
         return 'Wrong file type'
     elif request.method == 'GET':
         return render_template('pages/file_upload.jinja2', )
 
+
 @app.route('/stl-pricing/slice', methods=['POST'])
 def slicing():
     sleep(3)
-    state={
-        'price':50,
-        'successful':True,
+    state = {
+        'price': 50,
+        'successful': True,
     }
-    stateJson=json.dumps(state)
+    stateJson = json.dumps(state)
     return stateJson
 
 
 def executeFromFile(filename):
-    time=localtime()
-    gcoName=''
+    time = localtime()
+    gcoName = ''
     for i in range(3):
-        gcoName += '_'+str(time[i])
+        gcoName += '_' + str(time[i])
     gcoName += '.'.join(filename.split('.')[0:-1])
-    os.system('sudo sh '+CURA_SCRIPT_PATH+' '+gcoName)
+    os.system('sudo sh ' + CURA_SCRIPT_PATH + ' ' + gcoName)
+
 
 @app.route('/stream')
 def stream():
     return render_template('pages/stream.jinja2')
+
+
+@app.route('/stream/control', methods=['POST'])
+def streamControl():
+    '''Put here socket which connects to raspberry running server and control it'''
+    adress = request.form['adress']
+    command = request.form['command']
+
+    successful, message = sendCommand(adress, command)
+    data = {
+        'successful': successful,
+        'message': message,
+    }
+    print(data)
+    return json.dumps(data)
+
+
+def sendCommand(adress, data):
+    import socket
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        connection.connect((adress, STREAM_PORT))
+        msgSend = {'control': data}
+        connection.send(json.dumps(msgSend).encode())
+        msgRecv = connection.recv(1024)
+        msg_decoded = msgRecv.decode('utf8')
+        msg_decoded = json.loads(msg_decoded)
+        return msg_decoded['successful'], msg_decoded['message']
+
+    except Exception as e:
+        print(e)
+        return False, 'Could not connect to server'
+
 
 with app.test_request_context():
     url_for('static', filename='*')
--- static/js/stream.js
+++ static/js/stream.js
@@ -2,13 +2,30 @@
  * Created by Jakub on 20. 5. 2016.
  */
 function startStream() {
-showPrompt();
+    streamControl('start');
 }
 function stopStream() {
-showPrompt();
+    streamControl('stop');
 }
 
 function showPrompt() {
-    var adress = prompt('Type in IP adress','192.168.2.9');
-    console.log(adress);
+    var adress = prompt('Type in IP adress', '192.168.2.9');
+    return adress;
 }
+function streamControl(method) {
+    var adress = showPrompt();
+    if (adress != null) {
+        console.log('Calling JSON')
+        $.ajax({
+            type: 'POST',
+            url: '/stream/control',
+            success: ajaxCallback,
+            data: {adress: adress, command: method}
+        })
+    }
+
+}
+ajaxCallback = function (data) {
+    data_decoded = JSON.parse(data);
+    console.log(data_decoded);
+};
