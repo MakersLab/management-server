c7a37e237877f739462ac302908d9e57610d8f10
jakub_zika <kuba.zika@email.cz>
2016-05-25 12:34:04 +0200

    Absolute cura path.

--- cura.sh
+++ cura.sh
@@ -1,2 +1,2 @@
-A=$(sudo ./CuraEngine/build/CuraEngine -v ./uploads/temporary.stl -o $1.gcode 2>&1 | tail -3 | head -1 | cut -d" " -f3)
+A=$(sudo /home/pi/CuraEngine/build/CuraEngine -v ./uploads/temporary.stl -o $1.gcode 2>&1 | tail -3 | head -1 | cut -d" " -f3)
 python -c "print round($A/3600.0, 1)*200"
