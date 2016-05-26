e62c7b8dea60a72f11b5b6617380c9c9e0368bb2
jakub_zika <kuba.zika@email.cz>
2016-05-26 16:55:11 +0200

    Add api-key  in generatenames.

--- modules/generateNames.py
+++ modules/generateNames.py
@@ -5,10 +5,11 @@ def generateNames(PRINTERS_PATH):
         content = content.splitlines()
         for index, i in enumerate(content):
             info = {}
-            name, address = i.split(' ')
+            name, address,api_key = i.split(' ')
             info['name'] = name
             info['index'] = index
             info['address'] = address
+            info['api-key'] = api_key
             printers.append(info)
         f.close()
         return printers
