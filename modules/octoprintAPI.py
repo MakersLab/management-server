3687bd51191a6b885ad1ad42fca41baca6b7ab69
jakub_zika <kuba.zika@email.cz>
2016-05-27 12:12:11 +0200

    Fix.

--- modules/octoprintAPI.py
+++ modules/octoprintAPI.py
@@ -5,16 +5,17 @@ def sendFile(file_name, printer):
     url = 'http://' + printer['address'] + "/api/files/local"
 
     files = {'file': open('data/gcodes/' + file_name, 'rb')}
-    headers = {'X-Api-Key': printer['api_key']}
+    headers = {'X-Api-Key': printer['api-key']}
     r = requests.post(url, files=files, headers=headers)
     return r
 
 
 def startPrint(name, printer):
     url = 'http://' + printer + '/api/files/local/' + name
+    headers = {'X-Api-Key': printer['api-key']}
     command = {
         'select': True,
         'print': True,
     }
-    r = requests.post(url, params=command)
+    r = requests.post(url, params=command,headers=headers)
     return r
